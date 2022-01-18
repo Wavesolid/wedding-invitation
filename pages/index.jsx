58da8db75acf6cc10ca2d2a7f4e4dccd2a581177
GifinoThoriq <gifino92@gmail.com>
2022-01-18 09:48:24 +0700

    new config for fe and cover page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,24 @@
+import styles from '../styles/Index.module.css';
+import Cover from './Cover/Cover.jsx';
+import Intro from './Intro/Intro.jsx';
+import Surat from './Surat/Surat.jsx';
+
 export default function index()
 {
     return (
-        <h1>Sadam</h1>
+        <div>
+            <div className={styles.pageWrapper}>
+                <div className={styles.bannerWrapper}>
+                    Foto Banner
+                </div>
+            </div>
+
+            <div className={styles.columnMain}>
+                <Cover/>
+                <Intro/>
+                <Surat/>
+            </div>
+        </div>
+
     );
 }
