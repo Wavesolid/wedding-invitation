1af5e9e5e66dd6236d09d6702fdc85747562f9bd
GifinoThoriq <66865274+GifinoThoriq@users.noreply.github.com>
2022-01-22 20:20:56 +0700

    form ucapan

--- components/Ucapan/UcapanForm.jsx
+++ components/Ucapan/UcapanForm.jsx
@@ -1,13 +1,19 @@
 export default function UcapanForm() {
     return(
-        <div className="ml-[20px]">
-            <span className="font-bold text-[18px]">Form Ucapan</span>
-            {/* <form>
-                <input className=" w-[335px] h-[30px] rounded-[12.5px] border-[1px] border-[#F2C777] mb-[8px] bg-[transparent] placeholder:font-light placeholder:text-[10px] placeholder:text-[#F2C777] focus:border-[#F2C777]" type="text" placeholder="Nama Lengkap" />
-                <input type="text" placeholder="Domisili" />
-                <textarea placeholder="Kirim ucapan mu"></textarea>
-
-            </form> */}
+        <div className="ml-[10px]">
+            <span className="font-bold text-[18px] mb-[16px]">Form Ucapan</span>
+            <form className="flex flex-col">
+                <label className="mb-[8px]" for="nama">Nama</label>
+                <input className="w-[335px] h-[30px] mb-[16px] bg-[transparent] border-[1px] border-[#F2C777] pl-[12px] text-[12px] rounded-[15px] focus:outline-[0]" type="text" id="nama"/>
+                <label className="mb-[8px]" for="domisili">Domisili</label>
+                <input className="w-[335px] h-[30px] mb-[16px] bg-[transparent] border-[1px] border-[#F2C777] pl-[12px] text-[12px] rounded-[15px] focus:outline-[0]" type="text" id="domisli"/>
+                <label className="mb-[8px]" for="ucapan">Kirim ucapan mu</label>
+                <textarea className="w-[335px] h-[66px] mb-[16px] bg-[transparent] border-[1px] border-[#F2C777] p-[12px] text-[12px] rounded-[15px] focus:outline-[0]" type="text" id="ucapan">
+                </textarea>
+                <button className="w-[332px] h-[31px] bg-[#F2C777] rounded-[15px]">
+                    <span className=" text-[#0D0D0D] text-[12px] font-bold">Kirim Sekarang</span>
+                </button>
+            </form>
         </div>
     )
 }
