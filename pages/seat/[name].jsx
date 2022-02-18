9326127470e99fe7d40abbe60498628a8fc64250
Hizbullah, Sadam Muhamad <Wavesolid@users.noreply.github.com>
2022-02-18 16:30:48 +0700

    Adding page and logic for Seat page

---
+++ pages/seat/[name].jsx
@@ -0,0 +1,49 @@
+import styles from '../../styles/Index.module.css';
+import Layout from '../../components/Layout';
+import Banner from '../../components/Banner';
+import Seat from '../../components/Seat/Seat';
+
+export default function SeatPage(props)
+{
+    return (
+        <div className={styles.columnMain}>
+            <Seat props={props.responseJson}/>
+        </div>
+    )
+}
+
+SeatPage.getLayout = function getLayout(page)
+{
+    return (
+        <Layout>
+            <Banner />
+            {page}
+        </Layout>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { name } = context.query;
+
+    const response = await fetch(`http://localhost:3000/api/guest/${name}`);
+    const responseJson = await response.json();
+    const { data } = responseJson;
+
+    if (data === null) {
+        return {
+            notFound: true
+        }
+    } else if (data.isFilled === false) {
+        return {
+            redirect: {
+                destination: `/index/${data.name}`
+            }
+        }
+    } else {
+        return {
+            props: {
+                responseJson
+            }
+        }
+    }
+}
