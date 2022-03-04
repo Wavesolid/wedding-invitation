70295c86c45a1aca12eb46205a1b18198d24957a
Wavesolid <sadamuhiz40@gmail.com>
2022-03-04 19:04:16 +0700

    adding new style for loader

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -2,10 +2,11 @@ import styles from './Loader.module.css';
 
 export default function Loader(props){
     return(
-        <div>
-            <div className={styles.backdrop}></div>
-            <div className={`${styles.wrapperLoader} ${props.positionBox}`}>
-                <div className={styles.loader}>
+        <div className={styles.wrapperLoader}>
+            <div className={styles['loadingio-spinner-ripple-ov8s088nv9t']}>
+                <div className={styles['ldio-d20187tezng']}>
+                    <div></div>
+                    <div></div>
                 </div>
             </div>
         </div>
