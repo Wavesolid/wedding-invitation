358ee7722f8dd6ed227ab51fc2db395d7ffba019
gifinothoriq <gifino92@gmail.com>
2022-03-16 14:53:57 +0700

    fix audio player and minor changes animation

--- pages/home.jsx
+++ pages/home.jsx
@@ -7,15 +7,20 @@ import Layout from '../components/Layout';
 import Banner from '../components/Banner';
 import Ucapan from '../components/Ucapan/Ucapan';
 import Gallery from '../components/Gallery/Gallery';
-import AudioPlayer from '../components/Audio/AudioPlayer';
 import Footer from '../components/Footer/Footer';
 import { useState } from 'react';
 import Protokol from '../components/Protokol/Protokol';
+import AudioPlayer from '../components/Audio/AudioPlayer';
 
 export default function Home(props){
+
+  const [play, setPlay] = useState(
+    props.playAudio
+  );
+
   const [ucapan,setUcapan] = useState(
     props.ucapanResponseJson.ucapan
-  )
+  );
 
   return(
       <div className={styles.columnMain}>
@@ -27,6 +32,7 @@ export default function Home(props){
           <Gallery/>
           <Protokol/>
           <Footer/>
+          <AudioPlayer play={play}/>
       </div>
   )
 }
@@ -35,7 +41,6 @@ Home.getLayout = function getLayout(page) {
     return (
       <Layout>
         <Banner />
-        <AudioPlayer/>
         {page}
       </Layout>
     )
@@ -46,11 +51,13 @@ export async function getServerSideProps(context)
 {
   const {name} = context.query;
 
+  const playAudio = context.query.data !== undefined ? context.query.data : false ;
+
   const ucapanResponse = await fetch(`${process.env.BASE_URL}/api/UcapanHandler`,{
     method: 'GET'
   });
 
   const ucapanResponseJson = await ucapanResponse.json();
 
-  return {props:{ucapanResponseJson}}
+  return {props:{ucapanResponseJson,playAudio}}
 }
