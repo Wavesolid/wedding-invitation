3184e4d3da7181609c0f04b713ea375f9824a6a0
Sadamuhiz <sadamuhiz40@gmail.com>
2022-03-22 14:00:06 +0700

    removing context that unused

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,8 @@ import { SessionProvider } from "next-auth/react";
 import { useState } from 'react';
 import Router from 'next/router';
 import Loader from '../components/Loader/Loader';
+import {PlayMusicContextProvider} from '../contexts/playMusic-context';
+import {QrContextProvider} from '../contexts/QrContext';
 
 function MyApp({ Component, pageProps, router }) {
   const [load, setLoad] = useState(false);
@@ -23,21 +25,25 @@ function MyApp({ Component, pageProps, router }) {
   const getLayout = Component.getLayout || ((page) => page)
   return load? <Loader /> : getLayout(
     <SessionProvider>
-      <motion.div 
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            opacity: 0
-          },
-          pageAnimate: {
-            opacity: 1
-          },
-        }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
+      <QrContextProvider>
+        <PlayMusicContextProvider>
+          <motion.div 
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0
+              },
+              pageAnimate: {
+                opacity: 1
+              },
+            }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </PlayMusicContextProvider>
+      </QrContextProvider>
     </SessionProvider>
   )
 
