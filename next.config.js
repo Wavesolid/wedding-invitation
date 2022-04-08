fd908873145b2ec95967da290f4a88610f481b12
Hizbullah, Sadam Muhamad <sadamuhiz40@gmail.com>
2022-04-08 20:35:06 +0700

    changing url for different cases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const getDomain = () => {
+  if(process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    switch(process.env.NEXT_PUBLIC_VERCEL_ENV) {
+      case 'production':
+        return ''
+      default:
+        if(process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG === 'preview') {
+          return 'https://weddingneysagintano.vercel.app'
+        }
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    }
+  }
+} 
+
 module.exports = {
   reactStrictMode: true,
   env: {
-    BASE_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+    BASE_URL: process.env.VERCEL_URL ? getDomain() : 'http://localhost:3000'
   },
 }
