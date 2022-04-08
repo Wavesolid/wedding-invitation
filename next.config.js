d9d79bb036d468c640667cd63e1a965ba4d4da72
Hizbullah, Sadam Muhamad <sadamuhiz40@gmail.com>
2022-04-08 20:57:11 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-console.log(process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG);
 const getDomain = () => {
   if(process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    console.log(process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG);
     switch(process.env.NEXT_PUBLIC_VERCEL_ENV) {
       case 'production':
         return ''
