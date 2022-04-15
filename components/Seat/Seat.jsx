e99e3a04daaff56c15368f9ace619c00d9eec7d4
gifinothoriq <gifino92@gmail.com>
2022-04-15 16:04:58 +0700

    move qr compo and seat compo into one pages

--- components/Seat/Seat.jsx
+++ components/Seat/Seat.jsx
@@ -1,76 +1,30 @@
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router';
-import ConfirmModal from '../Modal/ConfirmModal'
+import QrCodeGenerator from "../QrCodes/GenerateQrCode";
 
 export default function Seat({props})
 {
     const { totalSouvenir } = props.data;
     const { seatNumber } = props.data;
-    const { isCheckIn } = props.data;
     const { name } = props.data;
-    
-    const [showModal,setShowModal] = useState()
-    const [dataCheck, setDataCheck] = useState({
-        isCheckIn: "Checked In",
-        checkInTime: Date.now()
-    })
-    
-    const router = useRouter();
-
-    useEffect(() => {
-        if(isCheckIn.toLowerCase() === 'pending') {
-            console.log(isCheckIn);
-            setShowModal(
-                {
-                    title: "Confirmation",
-                    content: "Check In digunakan sebagai tanda kehadiran anda pada acara ini dan hanya bisa tekan sekali. Apakah anda yakin?"
-                }
-            )
-        }
-    }, [isCheckIn]);
-
-    const confirmHandler = async (e) => {
-        const { name } = props.data
-
-        setDataCheck({
-            isCheckIn: "Checked In",
-            checkInTime: Date.now()
-        })
-
-        const response = await fetch('/api/checkInHandler', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name,
-                ...dataCheck
-            })
-        });
-
-        const responseJson = await response.json();
-
-        location.reload()
-    }
-
-    const cancelHandler = () => {
-        return router.replace(`/qrcode/${name}`);
-    }
 
     return (
         <div className="h-screen bg-merah flex flex-col items-center justify-center">
-            {showModal && <ConfirmModal title={showModal.title} content={showModal.content} positionBox={'md:!left-[38%]'} onConfirm={confirmHandler} onCancel={cancelHandler} />}
             <div>
                 <img src="/Icon/asset-batik-8.svg"/>
             </div>
                 {seatNumber.split(',').map((x) => 
-                    <h1 key={x.trim()} className='text-white text-[32px] flex flex-row'>{x.trim()}</h1>
+                    <h1 key={x.trim()} className='text-putih text-[24px] flex flex-row'>{x.trim()}</h1>
                 )}
             <div>
                 <img className='rotate-180' src="/Icon/asset-batik-8.svg"/>
             </div>
             <div>
-                <span className="text-white text-[32px]">Total Souvenir : {totalSouvenir}</span>
+                <span className="text-putih text-[24px]">Total Souvenir : {totalSouvenir}</span>
+            </div>
+            <div className='rounded-lg bg-merah p-[24px] my-0 mx-auto flex items-center justify-center text-putih'>
+                <div>Tunjukan barcode ini ke penerima tamu untuk mengkonfirmasi kehadiran anda</div>
+                <div className='bg-white p-[8px]'>
+                    <QrCodeGenerator name={name} size={100}/>
+                </div>
             </div>
         </div>
     )
---
+++ pages/checkin/[name].jsx
@@ -0,0 +1,49 @@
+import styles from '../../styles/Index.module.css';
+import Layout from '../../components/Layout';
+import Banner from '../../components/Banner';
+import CheckIn from '../../components/CheckIn/CheckIn';
+
+export default function CheckInPage(props)
+{
+    return (
+        <div className={styles.columnMain}>
+            <CheckIn props={props.responseJson}/>
+        </div>
+    )
+}
+
+CheckInPage.getLayout = function getLayout(page)
+{
+    return (
+        <Layout>
+            <Banner />
+            {page}
+        </Layout>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { name } = context.query;
+
+    const response = await fetch(`${process.env.BASE_URL}/api/guest/${name}`);
+    const responseJson = await response.json();
+    const { data } = responseJson;
+
+    if (data === null) {
+        return {
+            notFound: true
+        }
+    // } else if (data.isFilled === false) {
+    //     return {
+    //         redirect: {
+    //             destination: `/index/${data.name}`
+    //         }
+    //     }
+    } else {
+        return {
+            props: {
+                responseJson
+            }
+        }
+    }
+}
