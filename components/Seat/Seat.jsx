39c2600d9c0fe5a219d0ba706a5f88d84084d6be
gifinothoriq <gifino92@gmail.com>
2022-04-20 16:16:14 +0700

    changes on backoffice and email api

--- components/Seat/Seat.jsx
+++ components/Seat/Seat.jsx
@@ -18,10 +18,10 @@ export default function Seat({props})
                 <img className='rotate-180' src="/Icon/asset-batik-8.svg"/>
             </div>
             <div>
-                <span className="text-putih text-[24px]">Total Souvenir : {totalSouvenir}</span>
+                <span className="text-putih text-[24px]">Souvenir : {totalSouvenir}</span>
             </div>
-            <div className='rounded-lg bg-merah p-[24px] my-0 mx-auto flex items-center justify-center text-putih'>
-                <div>Tunjukan barcode ini ke penerima tamu untuk mengkonfirmasi kehadiran anda</div>
+            <div className='rounded-lg bg-merah p-[24px] my-0 mx-auto flex items-center justify-between text-putih'>
+                <div className="w-[50%]">Tunjukan barcode ini ke penerima tamu untuk mengkonfirmasi kehadiran anda</div>
                 <div className='bg-white p-[8px]'>
                     <QrCodeGenerator name={name} size={100}/>
                 </div>
