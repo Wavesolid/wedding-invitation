63ee0d11927677e6bc49a13208ea8c46e5a2d607
Hizbullah, Sadam Muhamad <sadamuhiz40@gmail.com>
2022-04-27 14:19:29 +0700

    changing flow for registered guest in index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,23 @@ import styles from '../styles/Index.module.css';
 import Cover from '../components/Cover/Cover';
 import Layout from '../components/Layout';
 import Banner from '../components/Banner';
+import { useRouter } from "next/router";
+import { useEffect } from 'react';
 
 export default function index()
 {
-    return (
-        <div className={styles.columnMain}>
-          <Cover/>
-        </div>
-    );
+  const router = useRouter();
+  console.log(router.pathname)
+  useEffect(() => {
+    if(router.pathname === '/') {
+      router.replace('/home');
+    }
+  })
+  return (
+      <div className={styles.columnMain}>
+        <Cover/>
+      </div>
+  );
 }
 
 index.getLayout = function getLayout(page) {
