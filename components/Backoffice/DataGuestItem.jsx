b8e570b052b9b3159205097e5597e9b23c2cb2f5
Hizbullah, Sadam Muhamad <sadamuhiz40@gmail.com>
2022-05-09 23:18:55 +0700

    adding feature sendWaMsg in backoffice

--- components/Backoffice/DataGuestItem.jsx
+++ components/Backoffice/DataGuestItem.jsx
@@ -43,6 +43,25 @@ export default function DataGuestItem(props){
 
     const dateFormat = new Date(guestEdit.checkInTime).toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
     const link = `${process.env.BASE_URL}/${guestEdit.slug}`;
+    const andRegex = new RegExp(/&/g);
+    const andUrlencoded = "%26"
+    const formattedName = guestEdit.name.split(" ");
+    const formattedSlug = guestEdit.slug.split("-");
+    if(andRegex.test(guestEdit.name)) {
+        formattedName.map((x, index) => {
+            if(andRegex.test(x)) {
+                formattedName[index] = andUrlencoded
+            }
+        })
+    } 
+    if(andRegex.test(guestEdit.slug)) {
+        formattedSlug.map((x, index) => {
+            if(andRegex.test(x)) {
+                formattedSlug[index] = andUrlencoded
+            }
+        })
+    } 
+    const waMsg = `Assalamualaikum+Warahmatullahi+Wabarakatuh+%0D%0A%0D%0AKepada+Yth.%0D%0ABapak%2FIbu%2FSaudara%2Fi+${formattedName.join(" ")}+%0D%0A%0D%0ABismillahirrahmanirrahim+%0D%0A%0D%0ADengan+memohon+rahmat+dan+ridho+Allah+SWT%2C+kami+bermaksud+untuk+menyelenggarakan+acara+pernikahan+kami%3A%0D%0A%0D%0Adrg.+Neysa+Almira+%26+Gintano+Scorpy+Sugihartono%2C+S.T+%0D%0A%0D%0Ayang+InsyaAllah+akan+dilaksanakan+pada%3A+%0D%0A%0D%0A%F0%9F%97%93Hari%2Ftgl+%3A%0D%0AMinggu%2C+29+Mei+2022+%0D%0A%0D%0A%F0%9F%95%9BPukul+%3A%0D%0A07.30+WIB+%7C+Akad+Nikah%0D%0A11.00+WIB+%7C+Resepsi%0D%0A%0D%0A%F0%9F%8F%A0Tempat+%3A%0D%0ASheraton+Grand+Jakarta%2C+Gandaria+City+Hotel%2C+3rd+Floor+%0D%0A%0D%0AKlik+disini+untuk+melihat+undangan+anda+%3A+%0D%0A%0D%0Aweddingneysagintano.me/${formattedSlug.join("-")}+%0D%0A%0D%0AMerupakan+suatu+kehormatan+dan+kebahagiaan+bagi+kami+apabila+Bapak%2FIbu%2FSaudara%2Fi+${formattedName.join(" ")}+berkenan+hadir+dan+memberikan+do%E2%80%99a+restu%0D%0A%0D%0AWassalamu%E2%80%99alaikum+Warahmatullahi+Wabarakatuh`
 
     return(
         <tr>
@@ -57,7 +76,7 @@ export default function DataGuestItem(props){
                 <div className="text-sm text-center text-gray-900">{guestEdit.email}</div>
             </td>
             <td className="px-6 py-4 whitespace-nowrap text-center">
-                <span className="text-sm text-gray-900"> {guestEdit.waNumber} </span>
+                <a className="text-indigo-600 hover:text-indigo-900" href={`https://api.whatsapp.com/send?text=${waMsg}&phone=${guestEdit.waNumber}`}> {guestEdit.waNumber} </a>
             </td>
             <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">{guestEdit.totalPerson}</td>
             <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
